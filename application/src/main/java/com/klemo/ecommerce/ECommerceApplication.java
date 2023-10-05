160cd0d6fe0b1f4daddcd6daa1d16ed4aec6bc5c
klemo <matuskoklemo@gmail.com>
2023-10-05 22:03:41 +0200

    fix: broken spring package scan

--- application/src/main/java/com/klemo/ecommerce/ECommerceApplication.java
+++ application/src/main/java/com/klemo/ecommerce/ECommerceApplication.java
@@ -2,10 +2,10 @@ package com.klemo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootConfiguration
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = "com.klemo.ecommerce")
 public class ECommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
