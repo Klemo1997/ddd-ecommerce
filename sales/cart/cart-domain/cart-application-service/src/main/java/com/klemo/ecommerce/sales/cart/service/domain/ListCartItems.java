2cd1a8d1269bdb591137e849e6d463d274d52ce1
klemo <matuskoklemo@gmail.com>
2023-10-22 22:08:35 +0200

    fix: Use CartItem DTO instead of domain entity in ListCartItemsResponse

--- sales/cart/cart-domain/cart-application-service/src/main/java/com/klemo/ecommerce/sales/cart/service/domain/ListCartItems.java
+++ sales/cart/cart-domain/cart-application-service/src/main/java/com/klemo/ecommerce/sales/cart/service/domain/ListCartItems.java
@@ -3,10 +3,13 @@ package com.klemo.ecommerce.sales.cart.service.domain;
 import com.klemo.ecommerce.sales.cart.service.domain.dto.ListCartItemsQuery;
 import com.klemo.ecommerce.sales.cart.service.domain.dto.ListCartItemsResponse;
 import com.klemo.ecommerce.sales.cart.service.domain.entity.Cart;
+import com.klemo.ecommerce.sales.cart.service.domain.entity.CartItem;
 import com.klemo.ecommerce.sales.cart.service.domain.port.output.CartRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -24,6 +27,21 @@ public class ListCartItems {
                     log.warn("Could not find cart with id: {}", query.cartId());
                     return new CartNotFoundException("Could not find cart with id: " + query.cartId());
                 });
-        return new ListCartItemsResponse(cart.getItems());
+        return new ListCartItemsResponse(cartItemsToCartItemDtos(cart.getItems()));
+    }
+
+    private List<com.klemo.ecommerce.sales.cart.service.domain.dto.CartItem> cartItemsToCartItemDtos(
+            List<CartItem> items) {
+        return items.stream().map(this::cartItemToCartItemDto).toList();
+    }
+
+    private com.klemo.ecommerce.sales.cart.service.domain.dto.CartItem cartItemToCartItemDto(CartItem cartItem) {
+        return new com.klemo.ecommerce.sales.cart.service.domain.dto.CartItem(
+            cartItem.getProductId().getValue(),
+            cartItem.getTitle(),
+            cartItem.getUnitPrice().amount(),
+            cartItem.getUnitPrice().currency().getCurrencyCode(),
+            cartItem.getQuantity().value()
+        );
     }
 }
--- sales/cart/cart-domain/cart-application-service/src/main/java/com/klemo/ecommerce/sales/cart/service/domain/dto/ListCartItemsResponse.java
+++ sales/cart/cart-domain/cart-application-service/src/main/java/com/klemo/ecommerce/sales/cart/service/domain/dto/ListCartItemsResponse.java
@@ -1,9 +1,6 @@
 package com.klemo.ecommerce.sales.cart.service.domain.dto;
 
-import com.klemo.ecommerce.sales.cart.service.domain.entity.CartItem;
-
 import java.util.List;
 
-// exposes domain entity to world, must be fixed..
 public record ListCartItemsResponse(List<CartItem> cartItemList) {
 }
