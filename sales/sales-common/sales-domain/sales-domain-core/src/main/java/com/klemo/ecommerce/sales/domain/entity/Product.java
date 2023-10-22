264b2827feab6afe38d25e25c5de1987abb3e63d
klemo <matuskoklemo@gmail.com>
2023-10-22 19:58:19 +0200

    feat: add Product entity to sales-domain-core

---
+++ sales/sales-common/sales-domain/sales-domain-core/src/main/java/com/klemo/ecommerce/sales/domain/entity/Product.java
@@ -0,0 +1,38 @@
+package com.klemo.ecommerce.sales.domain.entity;
+
+import com.klemo.ecommerce.domain.entity.Entity;
+import com.klemo.ecommerce.domain.value_object.Money;
+import com.klemo.ecommerce.domain.value_object.ProductId;
+
+public class Product extends Entity<ProductId> {
+
+    private String title;
+
+    private String description;
+
+    private Money price;
+
+    public Product(ProductId id) {
+        this.setId(id);
+    }
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
