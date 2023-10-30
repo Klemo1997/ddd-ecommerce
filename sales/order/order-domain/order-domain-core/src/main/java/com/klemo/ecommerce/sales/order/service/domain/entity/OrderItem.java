32e7b1e6022c564c65a5516df8f88b979b6185bb
klemo <matuskoklemo@gmail.com>
2023-10-30 15:53:10 +0100

    feat: add OrderItem entity base impl

--- sales/order/order-domain/order-domain-core/src/main/java/com/klemo/ecommerce/sales/order/service/domain/entity/OrderItem.java
+++ sales/order/order-domain/order-domain-core/src/main/java/com/klemo/ecommerce/sales/order/service/domain/entity/OrderItem.java
@@ -1,7 +1,32 @@
 package com.klemo.ecommerce.sales.order.service.domain.entity;
 
 import com.klemo.ecommerce.domain.entity.Entity;
+import com.klemo.ecommerce.domain.value_object.Money;
 import com.klemo.ecommerce.domain.value_object.OrderItemId;
+import com.klemo.ecommerce.domain.value_object.ProductId;
+import com.klemo.ecommerce.domain.value_object.Quantity;
 
 public class OrderItem extends Entity<OrderItemId> {
+    private final ProductId productId;
+    private final Quantity quantity;
+    private final Money price;
+
+    public OrderItem(OrderItemId id, ProductId productId, Quantity quantity, Money price) {
+        setId(id);
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public ProductId getProductId() {
+        return productId;
+    }
+
+    public Quantity getQuantity() {
+        return quantity;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
 }
