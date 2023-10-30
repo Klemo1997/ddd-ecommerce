1780920b136203cfbf22ec1b1db22c0905539f62
klemo <matuskoklemo@gmail.com>
2023-10-30 10:54:23 +0100

    feat: add warehouse domain core

--- common/common-domain/src/main/java/com/klemo/ecommerce/domain/value_object/Quantity.java
+++ common/common-domain/src/main/java/com/klemo/ecommerce/domain/value_object/Quantity.java
@@ -1,6 +1,8 @@
 package com.klemo.ecommerce.domain.value_object;
 
-public record Quantity(long value) {
+public record Quantity(long value) implements Comparable<Quantity> {
+    public static Quantity ZERO = new Quantity(0L);
+
     public Quantity {
         if (value < 0L) {
             throw new IllegalArgumentException("Value cannot be less than zero");
@@ -9,5 +11,20 @@ public record Quantity(long value) {
 
     public Quantity add(Quantity other) {
         return new Quantity(value + other.value);
+    }
+
+    public Quantity subtract(Quantity other) {
+        return new Quantity(value - other.value);
+    }
+
+    @Override
+    public int compareTo(Quantity o) {
+        if (o == null) throw new NullPointerException();
+
+        return Long.compare(value, o.value);
+    }
+
+    public boolean isEmpty() {
+        return equals(ZERO);
     }
 }
---
+++ warehouse/warehouse-domain/warehouse-application-service/src/main/java/com/klemo/ecommerce/warehouse/service/domain/Stock.java
@@ -0,0 +1,45 @@
+package com.klemo.ecommerce.warehouse.service.domain;
+
+import com.klemo.ecommerce.domain.entity.AggregateRoot;
+import com.klemo.ecommerce.domain.value_object.ProductId;
+import com.klemo.ecommerce.domain.value_object.Quantity;
+
+public class Stock extends AggregateRoot<ProductId> {
+    private Quantity quantity;
+
+    public Stock(ProductId productId) {
+        setId(productId);
+        quantity = Quantity.ZERO;
+    }
+
+    public Stock(ProductId productId, Quantity quantity) {
+        setId(productId);
+        this.quantity = quantity;
+    }
+
+    public boolean isEmpty() {
+        return quantity.isEmpty();
+    }
+
+    public Quantity getQuantity() {
+        return quantity;
+    }
+
+    public void add(Quantity quantity) {
+        this.quantity = this.quantity.add(quantity);
+    }
+
+    public void take(Quantity quantity) {
+        try {
+            this.quantity = this.quantity.subtract(quantity);
+        } catch (IllegalArgumentException e) {
+            throw new NotEnoughInStockException(e);
+        }
+    }
+
+    public static class NotEnoughInStockException extends RuntimeException {
+        public NotEnoughInStockException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
